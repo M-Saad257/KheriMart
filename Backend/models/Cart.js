177b798c987eb6e423f2ef5ac335c0dc74f92c11
Muhammad Saad <msaadi3806@gmail.com>
2025-11-12 10:19:41 +0500

    Delete Backend directory

--- Backend/models/Cart.js
+++
@@ -1,8 +0,0 @@
-import mongoose from 'mongoose';
-const cartSchema = new mongoose.Schema({
-  userId: { type: mongoose.Schema.Types.ObjectId, required: true },
-  productId: { type: mongoose.Schema.Types.ObjectId, required: true, ref: 'Product' },
-  quantity: { type: Number, default: 1 },
-});
-
-export default mongoose.models.Cart || mongoose.model('Cart', cartSchema);
